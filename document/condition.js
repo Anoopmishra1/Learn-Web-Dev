f536a1f08b4c158f56d5c49162262117a07407d6
Anoopmishra1 <anoopmishra72226@gmail.com>
2023-08-09 09:15:20 +0530

    Initial commit

---
+++ document/condition.js
@@ -0,0 +1,20 @@
+// question 1
+
+
+// let c=15;
+
+// if(c>=10 && c<=20 ){
+//     console.log("pearson lies in this age ")
+
+// }else{
+//     console.log("pearson not lies in this age ")
+// } 
+
+// question 2
+
+let weak = mon;
+switch(weak){
+    case mon:
+        con
+}
+
---
+++ document/conversion.js
@@ -0,0 +1,32 @@
+/*
+// Conversion on Number
+let age = true;
+// console.log(typeof age);
+// console.log(age);
+
+let valueOfNumber = Number(age);
+console.log(typeof valueOfNumber);
+console.log(valueOfNumber);
+
+// Output:
+// 33 => 33
+// 33anoop =>NaN
+// true => 1 || false => 0
+
+
+// Conversion on Boolean
+let isLoggedIn = "";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+// Output:
+// anoop => true
+// "" => false
+// 1 => true || 0 => false 
+
+*/
+
+
+
