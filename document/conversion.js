5479d4b28b5d6c1cbe8d54ee6f24a2084636bcee
Anoopmishra1 <anoopmishra72226@gmail.com>
2023-08-11 16:39:01 +0530

    Update conversion.js

--- document/conversion.js
+++ document/conversion.js
@@ -1,4 +1,6 @@
 /*
+
+
 // Conversion on Number
 let age = true;
 // console.log(typeof age);
@@ -14,6 +16,7 @@ console.log(valueOfNumber);
 // true => 1 || false => 0
 
 
+
 // Conversion on Boolean
 let isLoggedIn = "";
 let booleanIsLoggedIn = Boolean(isLoggedIn);
@@ -25,8 +28,53 @@ console.log(booleanIsLoggedIn);
 // anoop => true
 // "" => false
 // 1 => true || 0 => false 
-
 */
+ 
+
+//*****************************************String Method**********************************************************
+
+
+let userName = "code_demon__";
+console.log(userName[0]);
+console.log(userName .__proto__);
+console.log(userName.length);
+console.log(userName .toUpperCase());
+console.log(userName .charAt(6));
+console.log(userName .indexOf("d"));
+console.log(userName .substring(3 , 9));
+console.log(userName .slice(4));
+
+let anotherUserName = "   __XYZ__   "
+console.log(anotherUserName .trim());
+console.log(anotherUserName .trimStart());
+console.log(anotherUserName .trimEnd());
+
+ 
+console.log(anotherUserName .replace(anotherUserName , userName ));
+console.log(anotherUserName .split(4));
+
+
+c
+{}
+12
+CODE_DEMON__
+e
+2
+e_demo
+_demon__
+__XYZ__
+__XYZ__
+   __XYZ__
+code_demon__
+[ '   __XYZ__   ' ]
+
+
+
+
+
+
+
+
 
 
 
